cfdee9f63c3b4b8d513d32e2d182965ccce3b8e9
emircubukcu <cubukcuemir2@gmail.com>
2024-11-23 00:09:27 +0300

    nextJSFixV1

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -3,55 +3,55 @@ import { db } from "@/lib/db";
 import { RedirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-interface InviteCodePageProps{
-    params:{
-        inviteCode:string;
-    };
+interface InviteCodePageProps {
+    params: Promise<{
+        inviteCode: string;
+    }>;
 };
 
-const InviteCodePage = async({params}:InviteCodePageProps) => {
-    const profile=await currentProfile()
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+    const profile = await currentProfile()
 
-    
+
     const { inviteCode } = await params
 
-    if(!profile){
+    if (!profile) {
         return RedirectToSignIn;
     }
-    if(!inviteCode){
+    if (!inviteCode) {
         return redirect("/");
     }
 
-    const existingServer=await db.server.findFirst({
-        where:{
-            inviteCode:inviteCode,
-            members:{
-                some:{
-                    profileId:profile.id
+    const existingServer = await db.server.findFirst({
+        where: {
+            inviteCode: inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id
                 }
             }
         }
     })
-    if(existingServer){
+    if (existingServer) {
         return redirect(`/servers/${existingServer.id}`);
     }
 
-    const server=await db.server.update({
-        where:{
-            inviteCode:inviteCode,
+    const server = await db.server.update({
+        where: {
+            inviteCode: inviteCode,
         },
-        data:{
-            members:{
-                create:{
-                    profileId:profile.id
+        data: {
+            members: {
+                create: {
+                    profileId: profile.id
                 }
             }
         },
     })
-    if(server){
+    if (server) {
         return redirect(`/servers/${server.id}`)
     }
-    else{
+    else {
         return null
     }
 
