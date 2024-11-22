229ae7ab4ba4bdfd40855dcc48fc98c2252e4449
emircubukcu <cubukcuemir2@gmail.com>
2024-11-22 23:58:07 +0300

    esLincFixV4

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -11,16 +11,20 @@ interface InviteCodePageProps{
 
 const InviteCodePage = async({params}:InviteCodePageProps) => {
     const profile=await currentProfile()
+
+    
+    const { inviteCode } = await params
+
     if(!profile){
         return RedirectToSignIn;
     }
-    if(!params.inviteCode){
+    if(!inviteCode){
         return redirect("/");
     }
 
     const existingServer=await db.server.findFirst({
         where:{
-            inviteCode:params.inviteCode,
+            inviteCode:inviteCode,
             members:{
                 some:{
                     profileId:profile.id
@@ -34,7 +38,7 @@ const InviteCodePage = async({params}:InviteCodePageProps) => {
 
     const server=await db.server.update({
         where:{
-            inviteCode:params.inviteCode,
+            inviteCode:inviteCode,
         },
         data:{
             members:{
