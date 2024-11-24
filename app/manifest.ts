7eb6d2a1e8e83a7318a03aa511f5ef19d84c0570
emircubukcu <cubukcuemir2@gmail.com>
2024-11-24 21:02:59 +0300

    pwaSupportv8

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Next.js App',
@@ -9,12 +9,29 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
+    orientation:"landscape",
     icons: [
       {
-        src: '/favicon.ico',
+        src: '../public/icon.png',
         sizes: 'any',
-        type: 'image/x-icon',
+        type: 'image/png',
       },
     ],
+    screenshots: [
+      {
+        src: "../public/screenShot.png",
+        sizes: "640x320",
+        type: "image/png",
+        form_factor: "wide",
+        label: "Application"
+      },
+      {
+        src: "../public/screenShot2.png",
+        sizes: "640x320",
+        type: "image/png",
+        form_factor: "wide",
+        label: "Application"
+      }
+    ]
   }
 }
